fb8d497cc067d673a0801c610ab36c98d2e22792
Semper Solus <sempersolus5@gmail.com>
2019-01-21 15:51:32 -0600

    Uploaded the meatgraph

---
+++ meatgraph.pl
@@ -0,0 +1,369 @@
+meat(minidragon,familiar, ghost1).
+meat(minidragon,demon, ghost2).
+meat(minidragon,demon2, ghost3).
+meat(minidragon,demonlord, ghost4).
+meat(minidragon,astaroth, ghost5).
+meat(minidragon,pebble, woodman).
+meat(minidragon,rock2, woodman).
+meat(minidragon,rock3, stoneman).
+meat(minidragon,rock4, armor).
+meat(minidragon,silver, medusa).
+meat(minidragon,horse2, medusa).
+meat(minidragon,horse3, lamia).
+meat(minidragon,horse4, scylla).
+meat(minidragon,wererat,sprite).
+meat(minidragon,werewolf, sprite).
+meat(familiar,wererat, worm1).
+meat(pebble,wererat, frog).
+meat(minidragon,goblin,zombie).
+meat(slime,goblin, ptero1).
+meat(familiar,goblin, spider).
+meat(pebble,goblin, lizard1).
+meat(zombie,goblin, octopus).
+meat(minidragon,skeleton,flower).
+meat(minidragon,redbone,flower).
+meat(slime,skeleton, jaguar).
+meat(slime,redbone, jaguar).
+meat(familiar,skeleton, octopus).
+meat(pebble,skeleton, ptero1).
+meat(minidragon,frog, snake).
+meat(familiar,frog, flyman).
+meat(pebble,frog, mushroom1).
+meat(minidragon,crab1, crab1).
+meat(minidragon,tree1, tree1).
+meat(minidragon,turtle1, turtle1).
+meat(minidragon,mushroom1, flower).
+meat(slime,mushroom1, jaguar).
+meat(familiar,mushroom1, octopus).
+meat(pebble,mushroom1, ptero1).
+meat(minidragon,griffon, medusa).
+meat(minidragon,rhino, raven).
+meat(minidragon,spider1, beetle1).
+meat(slime,spider1, skeleton).
+meat(familiar,spider1, ptero1).
+meat(pebble,spider1, goblin).
+meat(minidragon,sword1, sword1).
+meat(familiar,sword1, turtle1).
+meat(minidragon,woodman, sword1).
+meat(minidragon,flyman, familiar).
+meat(slime,flyman, lizard1).
+meat(familiar,flyman, slime).
+meat(pebble,flyman, frog).
+meat(minidragon,slime, worm1).
+meat(minidragon,zombie, tree1).
+meat(slime,zombie, jaguar).
+meat(pebble,zombie, ptero1).
+meat(minidragon,octopus, barracuda).
+meat(slime,octopus, mushroom).
+meat(familiar,octopus, jaguar).
+meat(pebble,octopus, redbone).
+meat(minidragon,lizard1, diveeagle).
+meat(slime, lizard1,spider1).
+meat(familiar,lizard1, skeleton).
+meat(pebble,lizard1, greenslime).
+meat(minidragon,jaguar, wererat).
+meat(slime,jaguar, frog).
+meat(familiar,jaguar, pebble).
+meat(pebble,jaguar, octopus).
+meat(minidragon,flower, tree1).
+meat(slime,flower, jaguar).
+meat(minidragon,snake1, turtle1).
+meat(slime,snake1, pebble).
+meat(minidragon,medusa, sprite).
+meat(slime,medusa, rhino).
+meat(minidragon,worm1, eye1).
+meat(slime,worm1, oni).
+meat(minidragon,ptero1, silver).
+meat(slime,ptero1, octopus).
+meat(spider1,pebble,goblin).
+meat(spider1,wererat,ptero1).
+meat(spider1,goblin,jaguar).
+meat(spider1,skeleton,flyman).
+meat(spider1,frog,slime).
+meat(spider1,crab1,woodman).
+meat(spider1,mushroom1,flyman).
+meat(spider1,spider,mushroom1).
+meat(spider1,flyman,ptero1).
+meat(spider1,octopus,pebble).
+meat(spider1,lizard1,octopus).
+meat(spider1,jaguar,lizard1).
+meat(spider1,flower,flyman).
+meat(spider1,snake1,greenslime).
+meat(spider1,worm1,redbone).
+meat(spider1,eye1,ghoul).
+meat(spider1,ghost1,werewolf).
+meat(spider1,sprite,horse2).
+meat(spider1,ptero1,frog).
+meat(jaguar,goblin,pebble).
+meat(jaguar,skeleton,slime).
+meat(jaguar,redbone,greenslime).
+meat(jaguar,frog1,ptero1).
+meat(jaguar,mushroom1,slime).
+meat(jaguar,spider1,frog1).
+meat(jaguar,flyman,mushroom1).
+meat(jaguar,zombie,greenslime).
+meat(jaguar,octopus,lizard1).
+meat(jaguar,lizard1,flyman1).
+meat(jaguar,jaguar,skeleton).
+meat(jaguar,flower,greenslime).
+meat(jaguar,sprite,clayman).
+meat(jaguar,ptero1,goblin).
+meat(jaguar,diveeagle,goblin).
+meat(frog1,goblin,goblin).
+meat(frog1,skeleton,skeleton).
+meat(frog1,redbone,redbone).
+meat(frog1,frog1,octopus).
+meat(frog1,mushroom1,skeleton).
+meat(frog1,spider1,slime).
+meat(frog1,flyman,flyman).
+meat(frog1,zombie,redbone).
+meat(frog1,octopus,spider1).
+meat(frog1,lizard1,lizard1).
+meat(frog1,jaguar,jaguar).
+meat(frog1,flower,redbone).
+meat(frog1,ptero1,ptero1).
+meat(frog1,diveeagle,ptero1).
+meat(octopus,goblin,flyman).
+meat(octopus,skeleton,goblin).
+meat(octopus,frog1,spider1).
+meat(octopus,mushroom1,goblin).
+meat(octopus,spider1,pebble).
+meat(octopus,flyman,jaguar).
+meat(octopus,octopus,greenslime).
+meat(octopus,lizard1,frog1).
+meat(octopus,jaguar,ptero1).
+meat(octopus,diveeagle,lizard1).
+meat(skeleton,goblin,octopus).
+meat(skeleton,skeleton,frog1).
+meat(skeleton,frog1,goblin).
+meat(skeleton,mushroom1,frog1).
+meat(skeleton,spider1,jaguar).
+meat(skeleton,flyman,spider1).
+meat(skeleton,zombie,frog1).
+meat(skeleton,octopus,flyman).
+meat(skeleton,lizard1,mushroom1).
+meat(skeleton,jaguar,slime).
+meat(skeleton,ptero1,pebble).
+meat(skeleton,diveeagle,pebble).
+meat(flower,diveeagle,worm1).
+meat(flower,goblin,frog1).
+meat(flower,skeleton,lizard1).
+meat(flower,frog1,redbone).
+meat(flower,crab1,sprite).
+meat(flower,mushroom1,lizard1).
+meat(flower,spider1,flyman).
+meat(flower,greenslime,jaguar).
+meat(flower,zombie,rhino).
+meat(flower,flower,rhino).
+meat(flower,snake1,zombie).
+meat(flower,medusa,crab1).
+meat(flower, ghost1,babydragon).
+meat(worm1,flower,silver).
+meat(worm1,snake1,woodman).
+meat(rhino,pebble,stone).
+meat(rhino,silver,wererat).
+meat(rhino,wererat,imp).
+meat(rhino,goblin,redbone).
+meat(rhino,crab1,crab1).
+meat(rhino,turtle1,turtle1).
+meat(rhino,tree1,tree1).
+meat(rhino,griffon,medusa).
+meat(rhino,rhino,diveeagle).
+meat(rhino, woodman,woodman).
+meat(rhino,flyman,oni).
+meat(rhino,snake1,snake1).
+meat(snake1,pebble,mushroom1).
+meat(snake1,wererat,wererat).
+meat(snake1,goblin,goblin).
+meat(snake1,skeleton,redbone).
+meat(snake1,crab1,moth1).
+meat(snake1,diveeagle,diveeagle).
+meat(snake1,flower,zombie).
+meat(snake1,snake1,barracuda).
+meat(snake1,snake1,woodman).
+meat(diveeagle,goblin,mushroom1).
+meat(diveeagle,skeleton,pebble).
+meat(minidragon,oni, revenant).
+meat(minidragon,skeleton3,darkbloom).
+meat(minidragon,armorskeleton,strongestflower).
+meat(minidragon,frog4, hydra).
+meat(minidragon,catwoman, fairy).
+meat(minidragon,rakshasa, sylph).
+meat(minidragon,tree2, tree2).
+meat(minidragon,tree3, tree3).
+meat(minidragon,tree4, tree4).
+meat(familiar,tree1, crab).
+meat(minidragon,crab2, crab2).
+meat(minidragon,crab3, crab3).
+meat(minidragon,crab4, crab4).
+meat(familiar,crab1, griffon).
+meat(minidragon,turtle2, turtle2).
+meat(minidragon,turtle3, turtle3).
+meat(minidragon,turtle4, turtle4).
+meat(familiar,turtle1, medusa).
+meat(minidragon,mushroom3, darkbloom).
+meat(minidragon,mushroom4, strongestflower).
+meat(minidragon,chimera3, naga).
+meat(minidragon,chimera4, scylla).
+meat(familiar,griffon, sword1).
+meat(minidragon,dino1, harpy).
+meat(minidragon,dino3, garuuda).
+meat(familiar,rhino, zombie).
+meat(pebble,rhino, greenslime).
+meat(minidragon,spider3, beetle3).
+meat(minidragon,spider4, beetle4).
+meat(minidragon,sword2, sword2).
+meat(minidragon,sword3, sword3).
+meat(minidragon,sword4, sword4).
+meat(minidragon,clayman, sword1).
+meat(minidragon,stoneman, sword2).
+meat(minidragon,armor, sword4).
+meat(minidragon,fly3, demon).
+meat(minidragon,fly4, astaroth).
+meat(minidragon,greenslime, worm2).
+meat(minidragon,slime3, worm3).
+meat(minidragon,slime4, worm4).
+meat(minidragon,zombie2, tree1).
+meat(minidragon,zombie3, tree3).
+meat(minidragon,revenant, tree4).
+meat(minidragon,moth1, moth1).
+meat(minidragon,moth2, moth2).
+meat(minidragon,moth3, moth3).
+meat(minidragon,moth4, moth4).
+meat(minidragon,octopus2, barracuda).
+meat(minidragon,octopus3, piranha).
+meat(minidragon,octopus4, fish4).
+meat(minidragon,lizard3, thunderbird).
+meat(minidragon,lizard4, bird4).
+meat(minidragon,babydragon, harpy).
+meat(minidragon,dragon3, crow3).
+meat(minidragon,dragon4, garuuda).
+meat(minidragon,diveeagle, griffon).
+meat(slime,diveeagle, octopus).
+meat(minidragon,bird2, griffon).
+meat(minidragon,bird3, chimera).
+meat(minidragon,bird4, sphinx).
+meat(minidragon,jaguar3, catwoman).
+meat(minidragon,jaguar4, rakshasa).
+meat(minidragon,crow1, griffon).
+meat(minidragon,harpy, griffon).
+meat(minidragon,crow3, chimera).
+meat(minidragon,crow4, sphinx).
+meat(minidragon,snake2, turtle2).
+meat(minidragon,snake4, turtle4).
+meat(minidragon,ptero2, horse2).
+meat(minidragon,ptero4, horse4).
+meat(minidragon,worm2, eye1).
+meat(minidragon,worm3, eye2).
+meat(minidragon,worm4, eye4).
+meat(minidragon,eye1, eye1).
+meat(minidragon,eye2, eye2).
+meat(minidragon,eye3, eye3).
+meat(minidragon,eye4, eye4).
+meat(minidragon,lamia, fairy).
+meat(minidragon,naga, nymph).
+meat(minidragon,scylla, sylph).
+meat(minidragon,darkbloom, tree2).
+meat(minidragon,strongestflower, tree4).
+meat(redbone,crab,wererat).
+meat(turtle1,silver,griffon).
+meat(turtle1,wererat,medusa).
+meat(turtle1,goblin,imp).
+meat(turtle1,oni,imp).
+meat(turtle1,crab1,moth1).
+meat(turtle1,griffon,griffon).
+meat(turtle1,barracuda,moth1).
+meat(turtle1,diveeagle,griffon).
+meat(turtle1,flower,ghost1).
+meat(moth1,wererat,griffon).
+meat(moth1,goblin,horse2).
+meat(moth1,crab1,sword1).
+meat(moth1,turtle1,eye1).
+meat(moth1,tree1,medusa).
+meat(moth1,barracuda,sword1).
+meat(moth1,zombie,medusa).
+meat(moth1,moth1,tree1).
+meat(moth1,diveeagle,turtle1).
+meat(moth1,flower,medusa).
+meat(moth1,snake1,eye1).
+meat(barracuda,wererat,silver).
+meat(barracuda,werewolf,horse2).
+meat(barracuda,crab1,eye1).
+meat(barracuda,turtle1,moth1).
+meat(barracuda,tree1,sprite).
+meat(barracuda,griffon,raven).
+meat(barracuda,rhino,snake1).
+meat(barracuda,barracuda,worm1).
+meat(barracuda,zombie,familiar).
+meat(barracuda,ghoul,imp).
+meat(barracuda,moth1,sword1).
+meat(eye1,crab1,tree1).
+meat(eye1,woodman,medusa).
+meat(eye1,barracuda,tree1).
+meat(eye1,greenslime,imp).
+meat(eye1,zombie,griffon).
+meat(eye1,ghoul,griffon).
+meat(eye1,diveeagle,crab1).
+meat(eye1,snake1,sword1).
+meat(ghost1,silver,eye1).
+meat(ghost1,wererat,moth1).
+meat(ghost1,turtle1,sprite).
+meat(ghost1,tree1,turtle2).
+meat(ghost1,rhino,tree1).
+meat(ghost1,sword1,babydragon).
+meat(ghost1,woodman,babydragon).
+meat(ghost1,barracuda,medusa).
+meat(ghost1,snake1,sprite).
+meat(sword1,woodman,griffin).
+meat(sword1,barracuda,ghost1).
+meat(sword1,zombie,raven).
+meat(sword1,diveeagle,moth1).
+meat(sword1,flower,raven).
+meat(sword1,eye1,medusa).
+meat(sword1,snake1,tree1).
+meat(griffin,wererat,tree1).
+meat(griffin,goblin,woodman).
+meat(griffin,turtle1,raven1).
+meat(griffin,rhino,medusa).
+meat(griffin,zombie,eye1).
+meat(griffin,diveeagle,sprite).
+meat(griffin,flower,eye1).
+meat(griffin,snake1,raven).
+meat(griffin,ptero1,imp).
+
+
+
+
+
+
+
+
+
+boss1(minidragon, nike).
+boss1(slime, scarab).
+
+boss2(minidragon, anubis).
+
+boss3(minidragon, catwoman).
+
+
+meatchain(A, B, C):-
+init,
+meatchainmaker(A,B,[],C), 
+%append('Eater: ',A,A1),
+%append('Meat: ',C,C1),
+%append('Result: ',B,B1),
+append('meatlog.txt'), write(A), nl,write(C),nl,write(B),nl.
+%, reverse(Meatchain,C).
+
+init:-tell('meatlog.txt'), write('***** List of Meat *****'), nl, told.
+
+meatchainmaker(A, B, Was, [C]):-
+meat(A,C, B),
+\+member(A,Was).
+
+meatchainmaker(A,F,Was,[C|Meatchain]):-
+       meat(A,C,B),           
+       F \== B,
+       \+member(B,Was),
+       meatchainmaker(B,F,[A|Was],Meatchain).
